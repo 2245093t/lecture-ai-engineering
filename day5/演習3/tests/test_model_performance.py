2fade4811a52184183a2de8844f4dc02dd77438c
ballbat4 <{}>
2025-05-21 15:16:00 +0900

    firstコミット

---
+++ day5/演習3/tests/test_model_performance.py
@@ -0,0 +1,19 @@
+import time
+import joblib
+import numpy as np
+
+def test_model_accuracy():
+    # モデルとテストデータのパスは適宜修正
+    model = joblib.load("../../models/latest_model.pkl")
+    X_test = np.load("../../data/X_test.npy")
+    y_test = np.load("../../data/y_test.npy")
+    acc = model.score(X_test, y_test)
+    assert acc > 0.8  # 目標精度を設定
+
+def test_model_inference_time():
+    model = joblib.load("../../models/latest_model.pkl")
+    X_test = np.load("../../data/X_test.npy")
+    start = time.time()
+    _ = model.predict(X_test[:10])
+    elapsed = time.time() - start
+    assert elapsed < 1.0  # 10件で1秒以内など
